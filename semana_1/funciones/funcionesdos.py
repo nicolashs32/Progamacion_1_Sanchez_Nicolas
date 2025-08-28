cceaec45b678236d7a16a48651e1c0937fa67b28
nicolashs32 <nicolashs32.32@gmail.com>
2025-08-27 23:39:27 -0300

    funcionesdos

---
+++ semana_1/funciones/funcionesdos.py
@@ -0,0 +1,9 @@
+def operaciones(num1, num2):
+    print("Suma:", num1 + num2)
+    print("Resta:", num1 - num2)
+    print("Multiplicación:", num1 * num2)
+
+num1 = float(input("Por favor ingrese el primer número a operar: "))
+num2 = float(input("Ahora ingrese el segundo número a operar: "))
+
+operaciones(num1, num2)
